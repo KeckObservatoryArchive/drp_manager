2f4e9c4f9e5cb9b86140f17f75a11f99f3ca8a50
Jeff Mader <jmader@vm-koaserver2.(none)>
2022-04-20 08:20:36 -1000

    Fix ifs and PATH

--- pypeit_scripts/pypeit_lev2.sh
+++ pypeit_scripts/pypeit_lev2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-export PATH=$HOME/anaconda3/envs/pypeit/bin:/usr/sbin:/usr/bin:/sbin:/binmes
+export PATH=$HOME/.conda/envs/pypeit/bin:/usr/sbin:/usr/bin:/sbin:/binmes
 DATE=`date -u '+%Y%m%d'`
 INSTRUMENT=`echo $1 | tr '[a-z]' '[A-Z]'`
 LEV0DATA="/koadata/$INSTRUMENT/$DATE/lev0"
@@ -16,11 +16,11 @@ case $INSTRUMENT in
     RUN=false
     ;;
 esac
-if ! -d $LEV0DIR; then
+if [ ! -d $LEV0DIR ]; then
   echo "No lev0 data found in $LEV0DATA"
   RUN=false
 fi      
-if "$RUN"; then
+if [ "$RUN" ]; then
   cd /drp/manager/default/pypeit_scripts
   python pypeit_lev2.py $INSTRUMENT -i /koadata/$INSTRUMENT/$DATE/lev0 -r $PREFIX -o /k2drpdata/${INSTRUMENT}_DRP/$DATE -n 10 
 fi
