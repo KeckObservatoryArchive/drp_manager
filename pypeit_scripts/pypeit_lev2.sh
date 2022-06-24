dba344e5ca417f3e407a2119237ff48b225272fd
Jeff Mader <jmader@vm-koaserver2.(none)>
2022-06-24 08:38:12 -1000

    Added optional --calibonly second parameter

--- pypeit_scripts/pypeit_lev2.sh
+++ pypeit_scripts/pypeit_lev2.sh
@@ -20,7 +20,11 @@ if [ ! -d $LEV0DIR ]; then
   echo "No lev0 data found in $LEV0DATA"
   RUN=false
 fi      
+CALIB=''
+if [ "$2" == "--calibonly" ]; then
+    CALIB=$2
+fi
 if [ "$RUN" ]; then
   cd /drp/manager/default/pypeit_scripts
-  python pypeit_lev2.py $INSTRUMENT -i /koadata/$INSTRUMENT/$DATE/lev0 -r $PREFIX -o /k2drpdata/${INSTRUMENT}_DRP/$DATE -n 10 
+  python pypeit_lev2.py $INSTRUMENT -i /koadata/$INSTRUMENT/$DATE/lev0 -r $PREFIX -o /k2drpdata/${INSTRUMENT}_DRP/$DATE -n 10 $CALIB
 fi
