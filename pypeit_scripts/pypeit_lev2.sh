b195ad6c5fb86659236b0077d3908cede986e269
Jeff Mader <jeff.mader@gmail.com>
2023-05-10 09:37:18 -1000

    Update for multiple parameters - pypeit version and calibonly

--- pypeit_scripts/pypeit_lev2.sh
+++ pypeit_scripts/pypeit_lev2.sh
@@ -3,11 +3,10 @@
 DATE=`date -u '+%Y%m%d'`
 INSTRUMENT=`echo $1 | tr '[a-z]' '[A-Z]'`
 PYPEIT_VERSION="pypeit"
-if [ $# -eq 2 ]
+if [ $# -ge 2 ] && [ "$2" != "--calibonly" ]
 then
     PYPEIT_VERSION="pypeit_$2"
 fi
-echo $INSTRUMENT $PYPEIT_VERSION
 export PATH=$HOME/.conda/envs/$PYPEIT_VERSION/bin:/usr/sbin:/usr/bin:/sbin:/bin
 LEV0DATA="/koadata/$INSTRUMENT/$DATE/lev0"
 RUN=true
@@ -28,15 +27,18 @@ case $INSTRUMENT in
     RUN=false
     ;;
 esac
-if [ ! -d $LEV0DIR ]; then
+if [ ! -d $LEV0DIR ]
+then
   echo "No lev0 data found in $LEV0DATA"
   RUN=false
 fi      
 CALIB=''
-if [ "$2" = "--calibonly" ]; then
-    CALIB=$2
+if [ "$2" == "--calibonly" ] || [ "$3" == "--calibonly" ]
+then
+    CALIB="--calibonly"
 fi
-if [ "$RUN" ]; then
+if [ "$RUN" ]
+then
   cd /drp/manager/default/pypeit_scripts
   python pypeit_lev2.py $INSTRUMENT -i /koadata/$INSTRUMENT/$DATE/lev0 -r $PREFIX -o $OUTPUTDIR/${INSTRUMENT}_DRP/$DATE -n 10 $CALIB
 fi
