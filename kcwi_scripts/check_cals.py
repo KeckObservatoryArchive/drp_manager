cdad6fbf2cb74ecaaf7e0587107e3c3791b393fe
Jeff Mader <jeff.mader@gmail.com>
2025-04-23 07:46:46 -1000

    Wrapper for kcwidrp.check_cals

---
+++ kcwi_scripts/check_cals.py
@@ -0,0 +1,24 @@
+#!/usr/local/anaconda/bin/python
+
+import argparse
+import datetime as dt
+from os import system
+
+parser = argparse.ArgumentParser(description="Check KCWI calibrations")
+parser.add_argument("color", help="blue or red")
+args = parser.parse_args()
+
+if args.color not in ["blue", "red"]:
+    print("Parameter value must be blue or red")
+    exit()
+
+files = {"blue":"KB", "red":"KR"}
+
+utdate = dt.datetime.utcnow().strftime("%Y%m%d")
+
+cmd      = "/home/kcwidrp/.conda/envs/default/bin/check_cals"
+config   = "/k2drpdata/KCWI_DRP/configs/kcwi_lev1.cfg"
+files    = f"/koadata/KCWI/{utdate}/lev0/{files[args.color]}*.fits"
+full_cmd = f"{cmd} -c {config} {files}"
+print(full_cmd)
+system(full_cmd)
