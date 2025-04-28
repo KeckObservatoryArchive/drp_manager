99603f5b2388984c20c4c5cc3bd26ce487781bcb
Jeff Mader <jeff.mader@gmail.com>
2025-04-27 15:11:28 -1000

    Added main()

--- kcwi_scripts/check_cals.py
+++ kcwi_scripts/check_cals.py
@@ -4,21 +4,25 @@ import argparse
 import datetime as dt
 from os import system
 
-parser = argparse.ArgumentParser(description="Check KCWI calibrations")
-parser.add_argument("color", help="blue or red")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description="Check KCWI calibrations")
+    parser.add_argument("color", help="blue or red")
+    args = parser.parse_args()
 
-if args.color not in ["blue", "red"]:
-    print("Parameter value must be blue or red")
-    exit()
+    if args.color not in ["blue", "red"]:
+        print("Parameter value must be blue or red")
+        exit()
 
-files = {"blue":"KB", "red":"KR"}
+    files = {"blue":"KB", "red":"KR"}
 
-utdate = dt.datetime.utcnow().strftime("%Y%m%d")
+    utdate = dt.datetime.utcnow().strftime("%Y%m%d")
 
-cmd      = "/home/kcwidrp/.conda/envs/default/bin/check_cals"
-config   = "/k2drpdata/KCWI_DRP/configs/kcwi_lev1.cfg"
-files    = f"/koadata/KCWI/{utdate}/lev0/{files[args.color]}*.fits"
-full_cmd = f"{cmd} -c {config} {files}"
-print(full_cmd)
-system(full_cmd)
+    cmd      = "/home/kcwidrp/.conda/envs/default/bin/check_cals"
+    config   = "/k2drpdata/KCWI_DRP/configs/kcwi_lev1.cfg"
+    files    = f"/koadata/KCWI/{utdate}/lev0/{files[args.color]}*.fits"
+    full_cmd = f"{cmd} -c {config} {files}"
+    print(full_cmd)
+    system(full_cmd)
+
+if __name__ == "__main__":
+    main()
