68591627902993076469b750b6e978d7992f196d
Max Brodheim <max.brodheim@gmail.com>
2025-10-02 13:27:29 -1000

    push all changes to correct spot

--- pypeit_scripts/pypeit_lev2.py
+++ pypeit_scripts/pypeit_lev2.py
@@ -7,7 +7,9 @@ import requests
 from multiprocessing import Pool
 from argparse import ArgumentParser
 from configparser import ConfigParser
+import yaml
 import subprocess
+import numpy as np
 
 ###
 #### Bad Deimos detector. Temporary until this stops changing all the time.
@@ -43,36 +45,149 @@ def generate_pypeit_files(pargs, setup, cfg):
     # Create the setup object
     ps = setup.from_file_root(root, pargs.pypeit_name,
                                     extension=".fits")
-    ps.user_cfg += ['ignore_bad_headers = True']
-    if "deimos" in pargs.inst.lower() and deimos_det_5_is_bad:
-        ps.user_cfg += [f'detnum = {deimos_detnum}']
 
     # If the instrument is IR, use the -b flag (write_bkg_pairs=True)
-    ir_insts = cfg['INSTRUMENTS']['ir_insts'].split(' ')
-    is_ir = False
-    if pargs.inst in ir_insts:
-        print("Instrument is IR, using background pairs")
-        is_ir = True
+    is_ir = cfg['INSTRUMENTS'][pargs.inst]['ir']
 
     # Run the setup
-    # ps.run(setup_only=True, calibration_check=False, sort_dir=setup_dir,
-    #        obslog=True, write_bkg_pairs=is_ir)
     ps.run(setup_only = True, clean_config = True)
+
+    # Handle any instrument-specific configuration
+    inst_config = Path(__file__).parent / 'instrument_configs' / f'{str(pargs.inst).lower()}.yaml'
+    if inst_config.exists():
+        print(f"Applying instrument-specific configuration from {inst_config}")
+        with open(inst_config) as f:
+            cfg_inst = yaml.safe_load(f)
+        handle_instrument_config(cfg_inst, ps)
+    else:
+        print(f"No instrument-specific configuration found for {pargs.inst}")
+        print("Exiting...")
+        sys.exit(1)
+
     # Save the setup to .pypeit files
-    # ps.fitstbl.write_pypeit(setup_dir, configs='all', write_bkg_pairs=is_ir)
+    import pprint
+    pprint.pprint(ps.user_cfg)
     pypeit_files = ps.fitstbl.write_pypeit(output_path=setup_dir,
                                            cfg_lines=ps.user_cfg,
                                            write_bkg_pairs=is_ir,
                                            configs='all',
                                            version_override=None,
                                            date_override=None)
+def handle_instrument_config(cfg, ps):
+    """Adds user parameters from the config file to the PypeItSetup object.
+    This modifies the PypeItSetup object in place.
+
+    Parameters
+    ----------
+    cfg : ConfigParser
+        The configuration file parser object.
+    ps : PypeItSetup
+        The PypeItSetup object to modify.
+    """
+
+    # If there are any allowed keywords, keep only those files in the table
+    if 'allowed_keywords' in cfg:
+        total_mask = np.zeros(len(ps.fitstbl), dtype=bool)
+        for key in cfg['allowed_keywords']:
+            allowed_values = cfg['allowed_keywords'][key].split(',')
+            # create a mask to store which rows to keep
+            for value in allowed_values:
+                mask = ps.fitstbl[key] == value
+                if mask.any():
+                    total_mask |= mask
+        
+        print("Removing the following files:")
+        for i, row in enumerate(ps.fitstbl):
+            if not total_mask[i]:
+                print(f"    {row['filename']} because {key} = {row[key]}")
+        ps.fitstbl = ps.fitstbl[total_mask]
+        if len(ps.fitstbl) == 0:
+            print("No files left after applying allowed_keywords. Exiting...")
+            sys.exit(1)
+
+    # Add any user parameters from the config file
+    if 'user_cfg' in cfg:
+        lines = get_user_lines_from_dict(cfg['user_cfg'], lines=[])
+        ps.append_user_cfg(lines)
+
+    # If there are thresholds to apply, do that here
+    if 'fits_table_thresholds' in cfg:
+        for key in cfg['fits_table_thresholds']:
+            difference = float(cfg['fits_table_thresholds'][key])
+            print(f"Combining like values in {key} within {difference}")
+            combine_like_values(ps.fitstbl, key, difference)
+
+def get_user_lines_from_dict(cfg_dict, lines = [], bracket_level=1):
+    """Converts a dictionary of configuration parameters into a list of strings
+    that can be added to a PypeItSetup object.
+
+    Parameters
+    ----------
+    cfg_dict : dict
+        Dictionary of configuration parameters.
+
+    Returns
+    -------
+    list
+        List of strings in the format "key = value"
+    """
+
+    for key in cfg_dict:
+        if key == 'rdx':
+            print("Special handling of rdx")
+            for rdx_key in cfg_dict['rdx']:
+                line = f"{rdx_key} = {cfg_dict['rdx'][rdx_key]}"
+                lines.append(line)
+            continue
+        if isinstance(cfg_dict[key], dict):
+
+            pre_brackets = '[' * bracket_level
+            post_brackets = ']' * bracket_level
+            lines.append(f"{pre_brackets}{key}{post_brackets}")
+            get_user_lines_from_dict(cfg_dict[key], lines, bracket_level + 1)
+        else:
+            line = f"{key} = {cfg_dict[key]}"
+            lines.append(line)
+
+    return lines
+
+def combine_like_values(tbl, colname, difference):
+    """Combines values in a table column that should be considered the same.
+    For example, if the column is a dispersion angle, and two values are within
+    `difference`, they should be considered the same. This requires the sorting
+    of the table by the column first.
+    
+    This function modifies the table in place.
+
+    Parameters
+    ----------
+    tbl : PypeItMetaData
+        The input table containing the data to be combined.
+    colname : str
+        The name of the column to be processed.
+    difference : float
+        The maximum difference between values to be considered the same. 
+    """
+
+    tbl.sort(colname)
+
+    for i, value in enumerate(tbl[colname]):
+        # Skip the first one
+        if i == 0:
+            continue
+        # If the value is within difference of the previous value, set it to the previous value
+        if abs(value - tbl[colname][i - 1]) < difference:
+            tbl[colname][i] = tbl[colname][i - 1]
 
 def run_pypeit_helper(pypeit_file, pargs, cfg):
     """Runs a PypeIt reduction off of a specific .pypeit file, using the io
     parameters in pargs.
 
     The reduction is launched in a subprocess using the subprocess library, with
-    stdout and stderr directed to a single log file. 
+    stdout and stderr directed to a single log file.
+
+    This should ultimately be changed to use PypeIt's internal functions directly,
+    but this is easier for now.
 
     Parameters
     ----------
@@ -86,12 +201,11 @@ def run_pypeit_helper(pypeit_file, pargs, cfg):
 
     # Open file to dump logs into
     logpath = os.path.splitext(pypeit_file)[0] + '.log'
-    # logpath = os.path.join(pargs.output, logname)
     f = open(logpath, 'w+')
     
     # Get full output path
-    #outputs = os.path.join(pargs.output, os.path.splitext(pypeit_file)[0])
     outputs = os.path.splitext(pypeit_file)[0]
+
     # Run the reduction in a subprocess
     args = ['run_pypeit']
     args += [pypeit_file]
@@ -136,14 +250,11 @@ def alert_RTI(directory, pargs, cfg):
             return None
         return res
     
-    # data_directory = pargs.output + "/pypeit_files"
-    
     print(f"Alerting RTI that {directory} is ready for ingestion")
     url = cfg['RTI']['url']
 
     data = {
         'instrument': pargs.inst,
-        # 'koaid': "KOAID_HERE", # PypeIt files are found from datadir, not koaid
         'ingesttype': cfg['RTI']['rti_ingesttype'],
         'datadir': str(directory),
         'start': str(cfg.start_time),
@@ -162,20 +273,10 @@ def alert_RTI(directory, pargs, cfg):
 
 def get_config(cfg_file):
 
-    cfg = ConfigParser()
-    cfg.read(cfg_file)
-    
-    inst_options = cfg['INSTRUMENTS']['keck_inst_names'].split(' ')
-    inst_pypeit = cfg['INSTRUMENTS']['pypeit_inst_names'].split(' ')
-    inst_roots = cfg['INSTRUMENTS']['roots'].split(' ')
-    cfg.inst_opts = {
-        inst_options[i] : {
-            'pypeit_name' : inst_pypeit[i],
-            'root' : inst_roots[i]
-        }
-    for i in range(len(inst_options))}
+    with open(cfg_file) as f:
+            cfg = yaml.safe_load(f)
 
-    cfg.start_time = datetime.utcnow()
+    cfg['start_time'] = datetime.utcnow()
 
     return cfg
 
@@ -214,7 +315,7 @@ def get_parsed_args():
                         help='number of processes to launch')
     
     parser.add_argument('-c', '--config', dest='cfg_file',
-                        default='./pypeit_lev2.live.ini', help='Config file to use')
+                        default='./pypeit_lev2.live.yaml', help='Config file to use')
     
     parser.add_argument('--setup-only', dest='setup', action='store_true',
                         help="Only create the pypeit files, don't reduce them")
@@ -226,12 +327,13 @@ def get_parsed_args():
     parser.add_argument('--calibonly', dest='calib', action='store_true',
                         help='process calibrations only')
     
+    
     pargs =  parser.parse_args()
 
     return pargs
 
 def print_inst_options(cfg):
-    inst_options = "', ".join(cfg.inst_opts.keys())
+    inst_options = "', ".join(cfg['INSTRUMENTS'].keys())
     print(f"Options are: '{inst_options}'")
 
 def main():
@@ -255,17 +357,17 @@ def main():
         sys.exit(0)
 
     # Check if the input instrument name is valid
-    if pargs.inst not in cfg.inst_opts.keys():
+    if pargs.inst not in cfg['INSTRUMENTS'].keys():
         print("Invalid instrument name")
         print_inst_options(cfg)
         sys.exit(0)
     
     # Get PypeIt's instrument name
-    pargs.pypeit_name = cfg.inst_opts[pargs.inst]['pypeit_name']
+    pargs.pypeit_name = cfg['INSTRUMENTS'][pargs.inst]['pypeit_name']
 
     # If no root is specified, get it from the instruments list
     if pargs.root is None:
-        pargs.root = cfg.inst_opts[pargs.inst]['root']
+        pargs.root = cfg['INSTRUMENTS'][pargs.inst]['root']
 
 
     # Create all the pypeit files
@@ -274,25 +376,6 @@ def main():
     setup_files = Path(pargs.output) / 'pypeit_files'
     # Select only the pypeit files that are for an instrument configuration
     pypeit_files = list(setup_files.rglob(f'{pargs.pypeit_name}_?.pypeit'))
-    
-    # Add in special parameters
-    # For each pypeit file
-        # Open it
-        # Advance to user parameters
-        # Add in whatever we require
-        # Close and save
-    
-    pars = "[calibrations]\n[[flatfield]]\nsaturated_slits = mask\n"
-    
-    for file in pypeit_files:
-        with open(file, 'r+') as f:
-            contents = f.readlines()
-            for index, line in enumerate(contents):
-                if "# Setup" in line:
-                    contents.insert(index - 1, pars)
-                    break
-            f.seek(0)
-            f.writelines(contents)
             
     args = []
 
