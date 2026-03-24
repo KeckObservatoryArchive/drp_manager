d60b6bfc0805eafbcd457d36be6de6c580b13aca
jmader <jeff.mader@gmail.com>
2026-03-24 12:48:44 -0700

    Add use of cmd/checkcals_path

--- kcwi_scripts/check_cals.py
+++ kcwi_scripts/check_cals.py
@@ -3,14 +3,18 @@
 import argparse
 import datetime as dt
 import subprocess as sp
+from configparser import ConfigParser
 
 def check_cals(color, cron=False):
     files = {"blue":"KB", "red":"KR"}
 
     utdate = dt.datetime.utcnow().strftime("%Y%m%d")
 
-    cmd      = "/home/kcwidrp/.conda/envs/default/bin/check_cals"
-    config   = "/k2drpdata/KCWI_DRP/configs/kcwi_lev1.cfg"
+    cfg = ConfigParser()
+    cfg.read("/drp/manager/default/kcwi_scripts/kcwi.ini")
+
+    cmd      = cfg["cmd"]["checkcals_path"]
+    config   = "/drp/manager/default/configs/kcwi_lev1.cfg"
     files    = f"/koadata/KCWI/{utdate}/lev0/{files[color]}*.fits"
     full_cmd = f"{cmd} -c {config} {files}"
     if cron:
